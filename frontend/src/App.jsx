a52ab82291a585ae3a44b4806804a8688a419d70
manoj singh <manojsingh76543212@gmail.com>
2026-08-03 09:54:28 +0530

    Fix import casing for Navbar and Footer components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 import Home from "./pages/Home";
 import Doctors from "./pages/Doctors";
